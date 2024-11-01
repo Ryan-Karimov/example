3ea7f29729c887e76f4e984ee814e04950f8eb9c
Ravshanbek <ravshanbekkrmv@gmail.com>
2024-11-01 12:12:34 +0500

    merge with Kai's branch

--- src/apps/workspace/index.ts
+++ src/apps/workspace/index.ts
@@ -2,6 +2,9 @@ import { Router, NextFunction } from 'express';
 import { concatPaths, UploadFileHandler } from '../../helper'
 
 import { UPLOAD } from '../../config'
+import { WorkspaceController } from "./controller";
+import { createWorkspaceSchema, getWorkspacesSchema, updateWorkspaceSchema } from "./schema";
+import { WorkspaceService } from "./service";
 // import { UserService } from './service'
 // import { UserController } from './controller'
 // import { signUpSchema, signInSchema } from './schema'
@@ -9,22 +12,13 @@ import { UPLOAD } from '../../config'
 export function workspaceRouteRegister(prefix: string, router: Router, ...middlewares: Array<CallableFunction>): void {
     router.all(concatPaths(prefix), middlewares.map((middleware) => middleware()))
 
+    router.post(concatPaths(prefix, 'create'), WorkspaceController(createWorkspaceSchema, WorkspaceService.createWorkspace))
+    router.get(concatPaths(prefix, 'workspaces'), WorkspaceController(getWorkspacesSchema, WorkspaceService.getWorkspacesByOwnerId))
+    router.patch(concatPaths(prefix, 'workspaces'), WorkspaceController(updateWorkspaceSchema, WorkspaceService.updateWorkspaceById))
     router.get(concatPaths(prefix, ''), async (req, res) => {
         res.send({
             msg: "Response is succescfully!"
         })
     }
     );
-}
-
-import { Router } from "express";
-import { concatPaths } from "../../helper";
-import { WorkspaceController } from "./controller";
-import { createWorkspaceSchema, getWorkspacesSchema, updateWorkspaceSchema } from "./schema";
-import { WorkspaceService } from "./service";
-
-export function workspaceRoutes(prefix: string, router: Router): void {
-    router.post(concatPaths(prefix, 'create'), WorkspaceController(createWorkspaceSchema, WorkspaceService.createWorkspace))
-    router.get(concatPaths(prefix, 'workspaces'), WorkspaceController(getWorkspacesSchema, WorkspaceService.getWorkspacesByOwnerId))
-    router.patch(concatPaths(prefix, 'workspaces'), WorkspaceController(updateWorkspaceSchema, WorkspaceService.updateWorkspaceById))
 }
